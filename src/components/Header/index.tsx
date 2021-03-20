9fdce9b155bc483c53e8484cce9c04f6dae2360f
Guillermo <guillermoforerosuarez@gmail.com>
2021-03-20 16:03:03 -0500

    create Header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import Box from 'components/Box';
+import SearchBar from 'components/SearchBar';
+import React from 'react';
+import ic_ML from 'assets/Logo_ML2x.png'
+import styled from 'styled-components';
+
+const MeliLogo = styled.img`
+  height: 35px;
+  object-fit: contain;
+  margin-right: 10px;
+`;
+const Header: React.FC = () => (
+  <Box alignItems="center" justifyContent="center" height="60px" style={{backgroundColor: '#FFE600'}}>
+    <MeliLogo src={ic_ML} alt="search"/>
+    <SearchBar/>  
+  </Box>
+  
+);
+
+export default Header;
