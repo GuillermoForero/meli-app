392deb9a035fd9abc05edf42fe657e5867479fbf
Guillermo <guillermoforerosuarez@gmail.com>
2021-03-21 15:21:40 -0500

    create epic methods

---
+++ src/state/items/epics.ts
@@ -0,0 +1,50 @@
+import { Observable, from, of } from 'rxjs';
+import { mergeMap, catchError } from 'rxjs/operators';
+import { ofType, StateObservable } from 'redux-observable';
+import { AppState, EpicDependencies } from 'state';
+import { itemsTypes, itemsActions } from 'state/items';
+import { AxiosError } from 'axios';
+import { ReduxAction } from 'state/utils';
+
+const searchItemsEpic = (
+  action$: Observable<ReduxAction<string>>,
+  _: StateObservable<AppState>,
+  { meliClient }: EpicDependencies
+): Observable<any> =>
+  action$.pipe(
+    ofType(itemsTypes.SEARCH_ITEMS),
+    mergeMap(({ payload }) =>
+      from(meliClient.searchItems(payload)).pipe(
+        mergeMap(searchResult =>
+          of(itemsActions.searchItemsSuccess(searchResult))
+        ),
+        catchError((e: Error | AxiosError) => {
+          return of(itemsActions.searchItemsFailed(e.message));
+        }),
+      )
+    )
+  );
+
+  const getItemDetailEpic = (
+    action$: Observable<ReduxAction<string>>,
+    _: StateObservable<AppState>,
+    { meliClient }: EpicDependencies
+  ): Observable<any> =>
+    action$.pipe(
+      ofType(itemsTypes.SEARCH_ITEMS),
+      mergeMap(({ payload }) =>
+        from(meliClient.getItemDetail(payload)).pipe(
+          mergeMap(itemDetail =>
+            of(itemsActions.getItemDetailSuccess(itemDetail))
+          ),
+          catchError((e: Error | AxiosError) => {
+            return of(itemsActions.getItemDetailFailed(e.message));
+          }),
+        )
+      )
+    );
+
+export const itemsEpics = {
+  searchItemsEpic,
+  getItemDetailEpic,
+};
